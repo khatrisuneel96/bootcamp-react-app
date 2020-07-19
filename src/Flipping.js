95d9d050662a4511167ed0203aa90d35e4921db6
Suneel Kumar Khatri <khatrisuneel96@gmail.com>
2020-07-19 16:40:24 +0500

    Update Flipping.js

--- src/Flipping.js
+++ src/Flipping.js
@@ -16,9 +16,12 @@ function Flipping () {
         <div className={`box room ${isChanged}`}>
             <h1>Hi. It's is {isDay}</h1>
             <h3>Clicked : {count}</h3>
-            <button onClick={() => (
-                setCount(count + 1),setLight(!isLight),setBtn(!btn)
-            )}
+            <button onClick={() => {
+                return (
+                    setCount(count + 1),setLight(!isLight),setBtn(!btn)
+                    )
+                }
+            }
             >{
                 btn ? "Make Night" : "Make Day"
             }</button>
